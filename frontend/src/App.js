56eaf51b442486aa40401d33acf30e6485487ab1
Aizhan <proaizhan@gmail/com>
2025-07-07 23:09:31 +0500

    скрол

--- frontend/src/App.js
+++ frontend/src/App.js
@@ -3123,7 +3123,7 @@ export default function App() {
     } catch (error) {
         console.error("Ошибка при изменении блокировки:", error);
         showToastMessage("Не удалось изменить статус слота", "error");
-        setEntries(originalEntries); // Откат изменений в случае ошибки
+        setBlockedSlots(originalSlots);
     }
   };
 
